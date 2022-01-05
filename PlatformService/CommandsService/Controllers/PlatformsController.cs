409c60430e74d49d9bcb814a8f4310d14176de0e
Satya <satyap48@gmail.com>
2022-01-05 10:45:03 +0530

    Adding Services

---
+++ PlatformService/CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/commands/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController()
+        {
+
+        }
+
+        public ActionResult TestInboundRequest()
+        {
+            Console.WriteLine("--> Inbound POST # Command Service");
+
+            return Ok("Inbound request ok from Platforms Controller");
+        }
+    }
+}
